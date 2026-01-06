fab34a631bc1862ef45f0a14aeb997adaff6d05b
Dinesh <dineshm@2816@gmail.com>
2026-01-06 23:25:12 +0530

    Created basic structure

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <main className="max-w-6xl mx-auto px-6">
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
+  );
+}
